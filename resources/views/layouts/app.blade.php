6c77074fba332e417f85889f057993ce3614cb40
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-06-27 21:51:13 +0100

    Left with product purchase functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
     <title>@yield('title', 'Online Store')</title>
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
 </head>
@@ -13,14 +14,28 @@
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
-            <a class="navbar-brand" href="#">Online Store</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="{{ route('home.index') }}">Online Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link active" href="#">About</a>
+                    <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
+                    <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+                    <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
+                    <a class="nav-link active" href="{{ route('home.about') }}">About</a>
+                    <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                    @guest
+                        <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                        <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                    @else
+                        <form id="logout" action="{{ route('logout') }}" method="POST">
+                            <a role="button" class="nav-link active"
+                                onclick="document.getElementById('logout').submit();">Logout</a>
+                            @csrf
+                        </form>
+                    @endguest
                 </div>
             </div>
         </div>
@@ -38,7 +53,8 @@
     <div class="copyright py-4 text-center text-white">
         <div class="container">
             <small>
-                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank" href="https://twitter.com/danielgarax">
+                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+                    href="https://twitter.com/danielgarax">
                     Daniel Correa
                 </a> - <b>Paola Vallejo</b>
             </small>
